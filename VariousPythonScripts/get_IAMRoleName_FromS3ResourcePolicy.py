fd63c6b939ae2b99c06706e97c814fdf4882ebe1
SGWALLS <sgwalls@globe.life>
2022-02-10 10:43:33 -0600

    commit

--- VariousPythonScripts/get_IAMRoleName_FromS3ResourcePolicy.py
+++ VariousPythonScripts/get_IAMRoleName_FromS3ResourcePolicy.py
@@ -1,4 +1,3 @@
-import profile
 import boto3
 import json
 import time
@@ -12,20 +11,19 @@ def condition_check(statement_list):
             ['aws:userId'])        
         except KeyError:
             print('key error caught')
-    return pr_list
+    return pr_list if pr_list else None
 
 
 start_time = time.time()
-
-session = boto3.Session(profile_name="cdm_dev",region_name="us-west-2")
+bucket_name = input("Enter Bucket Name: ")
+profile_name = input("Enter Profile Name: ")
+session = boto3.Session(profile_name=profile_name,region_name="us-west-2")
 iam = session.resource('iam')
 s3 = session.resource('s3')
-bucket_policy = s3.BucketPolicy('tmk-cdm-data')
-try:
-    cond_list = [cond[0:21] for cond in 
-    condition_check(json.loads(bucket_policy.policy)['Statement']) if cond[0] == "A"] 
-except KeyError:
-    cond_list = None
+bucket_policy = s3.BucketPolicy(bucket_name)
+cond_list = [cond[0:21] for cond in 
+    condition_check(json.loads(bucket_policy.policy)['Statement']) 
+    if cond[0] == "A"] 
 principal_list = dict({k:v for k,v in [(role.role_id,role.role_name) 
                 for role in iam.roles.all() if cond_list and 
                 role.role_id in cond_list]}, 
@@ -39,11 +37,11 @@ print(principal_list)
 print("---  %s seconds ---" % (time.time()- start_time))
 
 # iam.meta.client('iam').simulate_principal_policy(
-#     PolicySourceArn='arn:aws:iam::896172592430:role/cdmdev_wsactuaries',
+#     PolicySourceArn='placeholder',
 #     ActionNames=[
 #         's3:GetObject',
 #     ],
 #     ResourceArns=[
-#         'arn:aws:s3:::tmk-cdm-data/raw/lnl-mainframe/alis-cvfile/current/*',
+#         'placeholder',
 #     ]
 # )
