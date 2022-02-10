c1458e62ec1a6176766c5a0da9c22f336d9b81ea
SGWALLS <sgwalls@globe.life>
2022-02-10 10:30:30 -0600

    updated to include Statement iteration and clean-up code

--- VariousPythonScripts/get_IAMRoleName_FromS3ResourcePolicy.py
+++ VariousPythonScripts/get_IAMRoleName_FromS3ResourcePolicy.py
@@ -3,19 +3,41 @@ import boto3
 import json
 import time
 
+
+def condition_check(statement_list):
+    pr_list = []
+    for statement in statement_list:
+        try:
+            pr_list.extend(statement['Condition']['StringNotLike']
+            ['aws:userId'])        
+        except KeyError:
+            print('key error caught')
+    return pr_list
+
+
 start_time = time.time()
 
 session = boto3.Session(profile_name="cdm_dev",region_name="us-west-2")
 iam = session.resource('iam')
 s3 = session.resource('s3')
 bucket_policy = s3.BucketPolicy('tmk-cdm-data')
-cond_list = [cond[0:21] for cond in json.loads(bucket_policy.policy)['Statement'][0]['Condition']['StringNotLike']['aws:userId'] if cond[0] == "A"] 
-# role_list = iam.roles.all()
-role_list = {k:v for k,v in [(role.role_id,role.role_name) 
-                for role in iam.roles.all()]}
-# user_list = iam.users.all()
-user_list = {k:v for k,v in [(user.user_id,user.user_name) 
-                for user in iam.users.all()]}
+try:
+    cond_list = [cond[0:21] for cond in 
+    condition_check(json.loads(bucket_policy.policy)['Statement']) if cond[0] == "A"] 
+except KeyError:
+    cond_list = None
+principal_list = dict({k:v for k,v in [(role.role_id,role.role_name) 
+                for role in iam.roles.all() if cond_list and 
+                role.role_id in cond_list]}, 
+                **{k:v for k,v in [(user.user_id,user.user_name) 
+                for user in iam.users.all() if cond_list and
+                user.user_id in cond_list]}
+            )
+
+print(principal_list)
+
+print("---  %s seconds ---" % (time.time()- start_time))
+
 # iam.meta.client('iam').simulate_principal_policy(
 #     PolicySourceArn='arn:aws:iam::896172592430:role/cdmdev_wsactuaries',
 #     ActionNames=[
@@ -27,17 +49,14 @@ user_list = {k:v for k,v in [(user.user_id,user.user_name)
 # )
 
 
-def get_principal(identifier):
-    if identifier[0:4] == "AROA":
-        return role_list[identifier] 
-    elif identifier[0:4] == "AIDA":
-        return user_list[identifier]
+# def get_principal(identifier):
+#     if identifier[0:4] == "AROA":
+#         return principal_list[identifier] 
+#     elif identifier[0:4] == "AIDA":
+#         return user_list[identifier]
 
-principal_access = {}
-for id in cond_list:
-    principal_access[id] = get_principal(id)
+# principal_access = {}
+# for id in cond_list:
+#     principal_access[id] = get_principal(id)
 
-print(principal_access)
-
-print("---  %s seconds ---" % (time.time()- start_time))
-
+# print(principal_access)
