2151d3a5d693fafa6d054a33eeb6a68d2d20e0f0
SGWALLS <sgwalls@globe.life>
2022-02-12 08:53:17 -0600

    commit

--- VariousPythonScripts/get_IAMRoleName_FromS3ResourcePolicy.py
+++ VariousPythonScripts/get_IAMRoleName_FromS3ResourcePolicy.py
@@ -15,12 +15,46 @@ def condition_check(statement_list):
 
 
 def resource_check(statement,target_value):    
-    if isinstance(statement['Resource'],list):
-        return (any(target_value in string for string in statement['Resource']) 
-            or "*" in statement['Resource'])        
-    else:
-        return (target_value in statement['Resource'] 
-                or statement['Resource'] == "*")
+    try:
+        if isinstance(statement['Resource'],list):
+            return (
+                        any(
+                            (target_value in string 
+                            for 
+                            string in statement['Resource']) 
+                            or 
+                            (
+                                ("*" in statement['Resource']) 
+                                and 
+                                (
+                                    ("s3:" in string for string in statement['Action'])
+                                    if 
+                                    isinstance(statement['Action'],list) 
+                                    else 
+                                    ("s3:" in statement['Action'])
+                                )   
+                            )
+                        )
+                    )        
+        else:
+            return (target_value in statement['Resource'] 
+                    or statement['Resource'] == "*")
+    except KeyError:
+        if isinstance(statement['NotResource'],list):
+            return (any((("s3:" in item for item in statement['Action']) or 
+                         ( "*" in statement['Action'])) and 
+                         (target_value not in string for string in 
+                          statement['NotResource'])) 
+                     or (("*" in statement['NotResource']) and 
+                         ("s3:" not in item for item in statement['Action']))
+                   )        
+        else:
+            return ((("s3:" in item for item in statement['Action']) and 
+                     (target_value not in statement['NotResource'])) #or 
+                    # (("s3:" not in item for item in statement['Action']) and
+                    #  (statement['NotResource'] == "*"))
+                    )
+        
 
 
 def write_file_out(data,filepath,filename):
@@ -41,8 +75,8 @@ while True:
         print('!!! Invalid Bucket Name !!!')
     else:
         break
-export_path = input("Enter path for file export: ").replace("\\","\\\\")
-export_name = input("Enter name for file export: ")
+# export_path = input("Enter path for file export: ").replace("\\","\\\\")
+# export_name = input("Enter name for file export: ")
 start_time = time.time()
 cond_list = [cond[0:21] for cond in 
     condition_check(json.loads(bucket_policy)['Statement']) 
@@ -54,13 +88,28 @@ principal_list = dict({k:v for k,v in [(role.role_id,role.role_name)
                 for user in iam.users.all() if cond_list and
                 user.user_id in cond_list]}
             )
-write_file_out(principal_list,export_path,export_name)
-print(principal_list)
+policy_list = iam.policies.filter(
+    Scope='All',
+    OnlyAttached=True,
+    PolicyUsageFilter='PermissionsPolicy'
+)
+for policy in policy_list:
+    access_switch = False
+    policy_doc = policy.default_version.document
+    if isinstance(policy_doc['Statement'],list):
+        for statements in policy_doc['Statement']:
+            access_switch = resource_check(statements,bucket_name)
+    else:
+        access_switch = resource_check(statements,bucket_name)    
+    if access_switch:
+        print(f"policy named: {policy.policy_name} has access to the target")
+# write_file_out(principal_list,export_path,export_name)
+# print(principal_list)
 
 print("---  %s seconds ---" % (time.time()- start_time))
 
-for statements in json.loads(bucket_policy)['Statement']:
-    print(resource_check(statements,bucket_name))
+# for statements in json.loads(bucket_policy)['Statement']:
+#     print(resource_check(statements,bucket_name))
 # iam.meta.client('iam').simulate_principal_policy(
 #     PolicySourceArn='placeholder',
 #     ActionNames=[
