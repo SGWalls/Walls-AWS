83744499ed75da4289050945316b1559a251f541
SGWALLS <sgwalls@globe.life>
2022-07-20 13:35:31 -0500

    update

--- aws_IAMTasks/IAM_Policies.py
+++ aws_IAMTasks/IAM_Policies.py
@@ -12,9 +12,43 @@ def import_file(filename, filedir=import_path):
     with open(statement_import) as json_file:
             return json.load(json_file) 
 
+
+def create_statement(
+    actions = None,
+    conditions = None,
+    effect = None,
+    not_actions = None,
+    not_principals = None,
+    not_resources = None,
+    principals = None,
+    resources = None,
+    sid = None
+):
+    values = {}
+    if actions is not None:
+       values["Action"] = actions
+    if conditions is not None:
+       values["Condition"] = conditions
+    if effect is not None:
+       values["Effect"] = effect
+    if not_actions is not None:
+       values["NotAction"] = not_actions
+    if not_principals is not None:
+       values["NotPrincipal"] = not_principals
+    if not_resources is not None:
+       values["NotResources"] = not_resources
+    if principals is not None:
+       values["Principal"] = principals
+    if resources is not None:
+       values["Resource"] = resources
+    if sid is not None:
+       values["Sid"] = sid
+    return values
+
 class Policies():
     createRole = import_file('IAM_IAM-CreateRole.json')
     passRole = import_file('IAM_IAM-PassRole.json')
     manageInstances = import_file('IAM_EC2-Create&MngInstances.json')
     manageSecGroups = import_file('IAM_EC2-SecurityGroup_Manage.json')
-
+    privateApiGw = import_file('IAM_API-Gateway_Private.json')
+    
--- aws_IAMTasks/IAM_PolicyGenerator.py
+++ aws_IAMTasks/IAM_PolicyGenerator.py
@@ -1,5 +1,4 @@
 import boto3
-# import aws_cdk.aws_iam as iam
 from aws_cdk.aws_iam import PolicyStatement, Effect as eff, PolicyDocument
 import os
 import time
@@ -12,16 +11,12 @@ from botocore.exceptions import UnauthorizedSSOTokenError
 from IAM_Policies import Policies
 
 
-def add_access(Sid=None,Effect=eff.DENY,Action=None,Resource=None,
+def add_access(Sid=None,Effect='Deny',Action=None,Resource=None,
                Principal=None,Condition=None, *args, **kwargs):
-    if Effect == "Allow":
-        Effect = eff.ALLOW
-    elif Effect == "Deny":
-        Effect = eff.DENY
     params = {}
     arg_dict = {
         "sid": Sid,
-        "effect": Effect,
+        "effect": eff.ALLOW if Effect == 'Allow' else eff.DENY,
         "actions": Action,
         "resources": Resource,
         "principals": Principal,
@@ -41,15 +36,6 @@ def add_access(Sid=None,Effect=eff.DENY,Action=None,Resource=None,
                 'principals','not_actions','not_resources','not_principals']):
             params[k] = [v]
     return PolicyStatement(**params)
-    # return PolicyStatement(
-    #     sid=Sid,
-    #     effect=Effect,
-    #     actions=Action if isinstance(Action,list) else [Action],
-    #     resources=Resource if isinstance(Resource,list) else [Resource],
-    #     principals=Principal,
-    #     conditions=Condition,
-    #     **arg_dict
-    # )
 
 access_input = input("Name of the Access Policies to use: ")
 access_list = access_input.split(',')
@@ -60,26 +46,8 @@ import_path = (
 )
 for file in access_list:
     statement_doc = getattr(Policies, file)
-#     statement_filename = file
-# # statement_filename = "IAM_testpol.json"
-#     statement_import = os.path.join(import_path, statement_filename)
-#     with open(statement_import) as json_file:
-#         statement_doc = json.load(json_file)
     if isinstance(statement_doc,list):
         for statement in statement_doc: 
-            # if statement['Effect'].upper() == 'ALLOW':
-            #     statement['Effect'] = eff.ALLOW
-            # else:
-            #     statement['Effect'] = eff.DENY
-            # iam_policy.add_statements(PolicyStatement(
-            #     sid=statement['Sid'],
-            #     principals=None,
-            #     effect=effect,
-            #     actions=statement['Action'] if isinstance(statement['Action'],list) 
-            #             else [statement['Action']],
-            #     resources=statement['Resource'] if isinstance(statement['Resource'],list) else [statement['Resource']],
-            #     conditions=statement['Condition'] if statement.get('Condition') else None
-            # ))
             iam_policy.add_statements(add_access(**statement))
     else:
         iam_policy.add_statements(add_access(**statement_doc))
